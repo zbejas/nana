2268829b2d1240fd13b60ec45de6b169081a8425
zbejas <zbejas@proton.me>
2026-04-29 10:46:28 +0200

    Wrap StorageSettings in a div for proper layout alignment in GeneralTab component

--- src/components/settings/GeneralTab.tsx
+++ src/components/settings/GeneralTab.tsx
@@ -10,7 +10,9 @@ export function GeneralTab() {
         <AutoSaveSettings />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <StorageSettings />
+        <div className="md:col-span-2">
+          <StorageSettings />
+        </div>
       </div>
     </div>
   );
